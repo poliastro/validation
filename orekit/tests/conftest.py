24effcff5ee1ed66137ee97d292dae8d0a586724
Jorge M.G <jorge@on-orbit.dev>
2021-01-29 10:57:21 +0100

    Better trace exceptions during data donwload

--- orekit/tests/conftest.py
+++ orekit/tests/conftest.py
@@ -5,5 +5,5 @@ from orekit.pyhelpers import download_orekit_data_curdir
 if not os.path.exists("orekit-data.zip"):
     try:
         download_orekit_data_curdir()
-    except Exception:
-        raise RuntimeError("Orekit data was not successfully downloaded.")
+    except Exception as e:
+        raise RuntimeError("Orekit data was not successfully downloaded") from e
